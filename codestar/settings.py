99585975644bd1aae9934901e8bbb22190c2d0df
mt-tahmasebi <50828142+mt-tahmasebi@users.noreply.github.com>
2026-05-29 13:04:45 +0100

    Update settings.py
    reset DEBUG back to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-*3cgz5h56wo%@1y&s#yfr4&0__gl!hfs4l59+&ljqv+35huu(+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1",]
 
